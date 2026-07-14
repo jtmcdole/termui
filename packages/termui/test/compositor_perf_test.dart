9a28bb38bfe3a20f220259bec527e24157c5cb63
John McDole <john@mcdole.org>
2026-07-13 19:39:25 -0700

    format

--- packages/termui/test/compositor_perf_test.dart
+++ packages/termui/test/compositor_perf_test.dart
@@ -5,17 +5,12 @@ void main() {
   test('Compositor flattens layers efficiently without Map allocations', () {
     final compositor = Compositor();
     final target = Buffer(80, 24);
-    
+
     // Create 5,000 overlapping layers
     final layers = List.generate(5000, (i) {
       final buffer = Buffer(10, 10);
       buffer.setAttributes(0, 0, char: 'A');
-      return LayeredBuffer(
-        buffer: buffer,
-        x: i % 70,
-        y: i % 14,
-        zIndex: i % 5,
-      );
+      return LayeredBuffer(buffer: buffer, x: i % 70, y: i % 14, zIndex: i % 5);
     });
 
     final watch = Stopwatch()..start();
